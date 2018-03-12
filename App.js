22616262c71ef4e7217f528102ea517bfe62809c
贾元发 <jiayuanfa@yeah.net>
2018-03-12 15:16:16 +0800

    完成轮播图功能

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import {
   View,
   ScrollView,
   Image,
+  Alert
 } from 'react-native';
 
 var ImageData = require('./data.json');
@@ -19,6 +20,8 @@ var ImageData = require('./data.json');
 // 获取屏幕宽度
 var Dimensions = require('Dimensions');
 var width = Dimensions.get('window').width;
+
+var timer;
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -33,18 +36,45 @@ type Props = {};
 // horizontal 是否水平显示该ScrollView
 export default class App extends Component<Props> {
 
+// 可变状态机
   state={
     currenge:0
+  }
+
+// 不可变状态机
+  static defaultProps={
+
+    // 间隔时间 单位是毫秒
+    duration:3000
+
   }
 
   render() {
     return (
       <View style={styles.container}>
       <ScrollView
+        ref="scrollView"
+
+        // 水平方向滚动
         horizontal={true}
+
+        // 不展示水平方向滚动条
         showsHorizontalScrollIndicator={false}
+
+        // 不展示垂直方向滚动条
         showsVerticalScrollIndicator={false}
+
+        // 分页展示 开
         pagingEnabled={true}
+
+        // 滚动结束后调用
+        onMomentumScrollEnd={(e)=>this.scrollEnd(e)}
+
+        // 手动拖拽后清空定时器
+        onScrollBeginDrag={()=>this.scrollBeginDrag()}
+
+        // 手动拖拽结束后重启定时器
+        onScrollEndDrag={()=>this.scrollEndDrag()}
       >
         {this.renderAllImage()}
       </ScrollView>
@@ -56,7 +86,81 @@ export default class App extends Component<Props> {
     );
   }
 
-// 小圆点数组
+  // 拖拽的时候清空定时器
+  scrollBeginDrag(){
+    // 拿到定时器
+    clearInterval(this.timer);
+  }
+
+  // 拖拽结束 重启定时器
+  scrollEndDrag(){
+    this.startTimer();
+  }
+
+  // UI加载完毕之后 启动定时器
+  componentDidMount(){
+
+    // 启动定时器
+    this.startTimer();
+
+  }
+
+  startTimer(){
+
+    // 拿到ScrollView
+    var scrollView = this.refs.scrollView;
+    var imageCount = ImageData.data.length;
+
+          // 这里下面的 this.state.currenge不能直接使用 所以在这里接收一下,但是这里获取没有用的，所以还要去里面获取，只不过换一种方式
+    // var stateCurrentPage = this.state.currenge;
+
+    // 这里真的是个坑，所以我们要转移坑
+    var obj = this;
+
+    // 设置定时器
+    this.timer = setInterval(function(){
+      // 设置当前显示哪一页
+      var currengPage = 0; // 临时存放目标currengPage
+
+      // 这里的this指的不是当前页对象。而是window，所以要转化 那么怎么搞？很简单在外面用一个对象接受this，而在这里面用this即可，就可以避免多出出现this指定对象不同造成的问题
+
+      // 判断是不是最后一个 如果是最后一个 就要跳转到第一页
+      if ((obj.state.currenge + 1) >= imageCount) {
+          // 清零
+          currengPage = 0;
+      }else{
+          // 让当前的currengPage等于状态机中的currengPage + 1 即向后面滚动一页
+          currengPage = obj.state.currenge + 1;
+      }
+
+      // 更新状态机
+      obj.setState({
+        currenge:currengPage
+      })
+
+      // 滚起来
+      var offsetX = currengPage * width;
+      scrollView.scrollTo({x:offsetX,y:0,animated:true});
+
+      console.log(currengPage);
+
+    },this.props.duration);
+  }
+
+  // 滚动结束后调用即可
+  scrollEnd(e){
+    // 滚动结束 通过e来计算出偏移量
+    var offserX = e.nativeEvent.contentOffset.x;
+    console.log(offserX);
+    // 求出当前页数 Math.floor是取整的意思
+    var currentPage = Math.floor(offserX/width);
+    // 更新状态机 就可以实现UI的自动刷新
+    this.setState({
+      currenge:currentPage
+    })
+  }
+
+  // 小圆点数组
   renderAllViewPage() {
     var style;
     var pagePointArray = [];
@@ -108,8 +212,6 @@ export default class App extends Component<Props> {
     return childView;
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
